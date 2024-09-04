135556be3f5e7b8367dc69397e54f56d3936aa0e
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2024-09-05 02:27:35 +0530

    dashboard setup done:

--- apps/server/src/controllers/AuthController.ts
+++ apps/server/src/controllers/AuthController.ts
@@ -1,20 +1,73 @@
+// import { Request, Response } from "express";
+// import prisma from "../config/db.config";
+// import jwt from "jsonwebtoken";
+// import "dotenv/config";
+
+// interface LoginPayloadType {
+//   name: string;
+//   email: string;
+//   image?: string;
+//   provider: string;
+//   oauth_id: string;
+// }
+
+// class AuthController {
+//   static async login(request: Request, response: Response) {
+//     try {
+//       const body: LoginPayloadType = request.body;
+//       let findUser = await prisma.user.findUnique({
+//         where: {
+//           email: body.email,
+//         },
+//       });
+
+//       if (!findUser) {
+//         findUser = await prisma.user.create({ data: body });
+//       }
+
+//       let JWTPayload = {
+//         name: body.name,
+//         email: body.email,
+//         id: findUser.id,
+//       };
+
+//       const token = jwt.sign(JWTPayload, process.env.SECRET_KEY!, {
+//         expiresIn: "15d",
+//       });
+
+//       return response.json({
+//         message: "LoggedIn successfully",
+//         user: {
+//           ...findUser,
+//           token: `Bearer ${token}`,
+//         },
+//       });
+//     } catch (error) {
+//       response.status(500).json({
+//         message: "Something went Wrong, please try again",
+//       });
+//     }
+//   }
+// }
+
+// export default AuthController;
+
 import { Request, Response } from "express";
-import prisma from "../config/db.config";
 import jwt from "jsonwebtoken";
-import "dotenv/config";
+import prisma from "../config/db.config";
 
 interface LoginPayloadType {
   name: string;
   email: string;
-  image: string;
-  provider: string;
   oauth_id: string;
+  provider: string;
+  image: string;
 }
 
 class AuthController {
-  static async login(request: Request, response: Response) {
+  static async login(req: Request, res: Response) {
     try {
-      const body: LoginPayloadType = request.body;
+      const body: LoginPayloadType = req.body;
       let findUser = await prisma.user.findUnique({
         where: {
           email: body.email,
@@ -22,30 +75,29 @@ class AuthController {
       });
 
       if (!findUser) {
-        findUser = await prisma.user.create({ data: body });
+        findUser = await prisma.user.create({
+          data: body,
+        });
       }
-
       let JWTPayload = {
         name: body.name,
         email: body.email,
         id: findUser.id,
       };
-
-      const token = jwt.sign(JWTPayload, process.env.SECRET_KEY!, {
-        expiresIn: "15d",
+      const token = jwt.sign(JWTPayload, process.env.JWT_SECRET!, {
+        expiresIn: "365d",
       });
-
-      return response.status(200).json({
-        message: "LoggedIn successfully",
+      return res.json({
+        message: "Logged in successfully!",
         user: {
           ...findUser,
           token: `Bearer ${token}`,
         },
       });
     } catch (error) {
-      response.status(500).json({
-        message: "Something went Wrong",
-      });
+      return res
+        .status(500)
+        .json({ message: "Something went wrong.please try again!" });
     }
   }
 }
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,18 @@
 import express, { Application } from "express";
 import cors from "cors";
 import "dotenv/config";
+import router from "./routes";
+
+const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
-const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(8080, () => {
+app.use("/api", router);
+
+app.listen(PORT, () => {
   console.log(`Server listening at ${PORT}`);
 });

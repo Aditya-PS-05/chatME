0fa1be5cc26f9d28b551f1bf1501217484f24e79
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2024-09-08 15:10:59 +0530

    errors corrected

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -6,9 +6,12 @@ import authMiddleware from "../middlewares/AuthMiddleware";
 const router = Router();
 
 router.post("/auth/login", AuthController.login);
-router.post("/chat-group", authMiddleware, ChatGroupController.store);
+
+// Chat Group Routes
+router.get("/chat-group", authMiddleware, ChatGroupController.index);
 router.get("/chat-group/:id", ChatGroupController.show);
 router.post("/chat-group", authMiddleware, ChatGroupController.store);
 router.put("/chat-group/:id", authMiddleware, ChatGroupController.update);
 router.delete("/chat-group/:id", authMiddleware, ChatGroupController.destroy);
+
 export default router;
